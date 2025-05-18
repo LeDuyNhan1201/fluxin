880142a5443827d42f11b5be60bb36253d45e698
LeDuyNhan1201 <benlun99999@gmail.com>
2025-05-18 21:10:44 +0700

    init commit

---
+++ backend/src/main/java/org/neb/fluxin/FluxinApplication.java
@@ -0,0 +1,13 @@
+package org.neb.fluxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/neb/fluxin/FluxinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neb.fluxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
